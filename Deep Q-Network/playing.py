b8a06264b5fef62b4987b60ca4e002b6cf6ee1db
Christopher Keibel <christopher.keibel.90@gmail.com>
2021-01-16 10:09:52 +0100

    GIF function

--- Deep Q-Network/playing.py
+++ Deep Q-Network/playing.py
@@ -1,4 +1,6 @@
 import gym
+from matplotlib import animation
+import matplotlib.pyplot as plt
 import time
 import numpy as np
 import tensorflow as tf
@@ -11,8 +13,22 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.losses import Huber
 from tensorflow.keras.optimizers import RMSprop
 
+""" creating GIF """
+def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
 
+    #Mess with this to change frame size
+    plt.figure(figsize=(frames[0].shape[1] / 72.0, frames[0].shape[0] / 72.0), dpi=72)
 
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
+    anim.save(path + filename, writer='imagemagick', fps=60)
+
+""" Deep Q-Network """
 # Funktion zum erstellen eines neuronalen Netzes
 def build_neural_net(INPUT_SHAPE, OUTPUT_SHAPE, LOSS_FUNCTION, OPTIMIZER):
     net_input = Input(shape=INPUT_SHAPE)
@@ -23,7 +39,7 @@ def build_neural_net(INPUT_SHAPE, OUTPUT_SHAPE, LOSS_FUNCTION, OPTIMIZER):
     x = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
     x = Activation("relu")(x)
     x = Flatten()(x)
-    x = Dense(256)(x)
+    x = Dense(512)(x)
     x = Activation("relu")(x)
     net_output = Dense(OUTPUT_SHAPE)(x)
 
@@ -33,11 +49,12 @@ def build_neural_net(INPUT_SHAPE, OUTPUT_SHAPE, LOSS_FUNCTION, OPTIMIZER):
     return model
 
 
-
+""" Environment """
 # Create Environment
 from Wrappers import make_env
 env = make_env("Pong-v0")
 
+""" DQN Parameters"""
 # DQN und Tagret Net Parameters
 INPUT_SHAPE = (84, 84, 4) # (Höhe, Breite, Frames)
 OUTPUT_SHAPE = env.action_space.n # Anzahl der möglichen Aktionen
@@ -45,19 +62,23 @@ LOSS_FUNCTION = Huber()
 OPTIMIZER = RMSprop(learning_rate=0.00025, rho=0.95, epsilon=0.01)
 
 DQN = build_neural_net(INPUT_SHAPE, OUTPUT_SHAPE, LOSS_FUNCTION, OPTIMIZER)
-DQN.load_weights("WEIGHTS/Pong-v0_DQN_End.h5")
-
-EPISODES = 10
+DQN.load_weights("WEIGHTS/Pong-v0_DQN_Ep_13058.h5")
 
 
+""" Playing """
+frames = [] # for a GIF
 
+EPISODES = 1
 for i in range(EPISODES):
     state = env.reset()
     done = False
     while not done:
+        env.render()
+        frames.append(env.render(mode="rgb_array"))
         action = np.argmax(DQN.predict(state))
         state, reward, done, info = env.step(action)
-        env.render()
+        #env.render()
         #time.sleep(.1)
         
-
+env.close()
+save_frames_as_gif(frames)
